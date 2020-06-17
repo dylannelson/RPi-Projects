c90e952f3b560f665fc187ae7fdb5692b8f1a7c9
Dylan Nelson <44685561+dylannelson@users.noreply.github.com>
2020-06-17 13:04:06 -0700

    updated with informational text

--- 8x8_LED_Matrix/txt.py
+++ 8x8_LED_Matrix/txt.py
@@ -1,12 +1,15 @@
 import board
 from adafruit_ht16k33.matrix import Matrix8x8
 import time
-
-i2c = board.I2C()
-matrix = Matrix8x8(i2c)
-
 import numpy as np
 from PIL import Image
+
+# basic setup for connecting to LED matrix through GPIO
+i2c = board.I2C()
+matrix = Matrix8x8(i2c)
+# Array that looks like the text for UCSD
+# 1's are a lit LED
+# 0's are an unlit LED
 x = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
               [1,0,0,0,1,0,0,1,1,1,0,0,1,1,1,0,1,1,1,0,0,0,0,0],\
               [1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0],\
@@ -16,11 +19,23 @@ x = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
               [1,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0],\
               [0,1,1,1,0,0,0,1,1,1,0,1,1,1,0,0,1,1,1,0,0,0,0,0]])
 slices = []
+
+# this loop makes a set of each different 8x8 slice from the above
+# array, in order from left to right this is done so each 8x8 can 
+# be displayed, and give it the text sliding effect
 for i in range(x.shape[1]):
     indices = range(i,i+8)
+    # this is a useful numpy funcctions that allow you to take
+    # slices of a certain length, and if you reach the end of 
+    # the array, it will wrap around to the beginning
     neighbourhood = x.take(indices, mode='wrap')
     x_slice = x.take(indices, mode='wrap',axis=1)
     slices.append(x_slice)
+
+# this, while looking very innefficient, is about as fast as the 
+# adafruit 'matrix.image' function, which I show in txt2.py
+
+# it basically powers each pixel on/off 1 by 1 from he slices we made
 while True:
     for i in slices:
         for h in range(8):
--- 8x8_LED_Matrix/txt2.py
+++ 8x8_LED_Matrix/txt2.py
@@ -1,13 +1,16 @@
 import board
 from adafruit_ht16k33.matrix import Matrix8x8
 import time
-
-i2c = board.I2C()
-matrix = Matrix8x8(i2c)
-
 import numpy as np
 from PIL import Image
-x = np.asarray(dtype=np.dtype('uint8'), a=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+i2c = board.I2C()
+matrix = Matrix8x8(i2c)
+# info for this function identicalt to info availible in txt.py, up until the last loop
+# *note* that here we use a different numpy array initialization.
+# this is due to PIL handling numpy arrays poorly, and making it this specific datatype helps
+# more info in the stackoverflow link a the end
+x = np.asarray(dtype=np.dtype('uint8'),\
+           a=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
               [1,0,0,0,1,0,0,1,1,1,0,0,1,1,1,0,1,1,1,0,0,0,0,0],\
               [1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0],\
               [1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0],\
@@ -22,10 +25,17 @@ for i in range(x.shape[1]):
     neighbourhood = x.take(indices, mode='wrap')
     x_slice = x.take(indices, mode='wrap',axis=1)
     slices.append(x_slice)
+
+# here we actually just turn each  slice into an image    
 for i in range(10):
     for cut in slices:
+        # due to poor design of how Pillows creates iamges from arrays, we have to
+        # create it as an 8 bit/pixel image, and then convert to 1 bit
         i = Image.fromarray(cut*255, mode='L').convert('1')
+        # and then just display it
         matrix.image(i)
+
+# here is further info on how/why PIL opperates so strangely with numpy arrays
 # https://stackoverflow.com/questions/32159076/python-pil-bitmap-png-from-array-with-mode-1
 
         
